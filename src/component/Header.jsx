c3f31e2d448e4daf11b42b29b5feb68b33b8e1f7
Ashish Verma <ashishkv845@gmail.com>
2024-03-17 23:37:44 +0530

    update header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,16 +7,22 @@ import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import axios from "axios";
 
 function Header() {
-  
+  const [show, setShow] = useState(false);
   return (
-    <header className="py-2 px-10 border shadow-lg items-center flex justify-between">
+    <header className="py-2 px-10  relative shadow-lg items-center flex justify-between">
       <div>
         <Link to="/">
           <img src={logo} alt="" className="w-[170px]     object-contain" />
         </Link>
       </div>
-      <div className="">
-        <ul className="flex md:gap-12 text-lg font-semibold">
+      <div className="  md:block">
+        <ul
+          className={`flex flex-row   md:gap-12  text-lg font-semibold ${
+            show
+              ? " flex-col absolute top-[70px] pt-10 bg-opacity-80 backdrop-blur-sm right-0 px-10 gap-5 z-10 bg-white w-full pb-10"
+              : "hidden md:flex"
+          }`}
+        >
           <li>
             <NavLink
               to="/"
@@ -32,7 +38,14 @@ function Header() {
           </li>
 
           <li className="relative  cursor-pointer">
-            <NavLink to="/category">Category</NavLink>
+            <NavLink
+              to="/category"
+              className={({ isActive }) =>
+                `${isActive ? "text-primary" : "text-inherit"}`
+              }
+            >
+              Category
+            </NavLink>
           </li>
           <li>
             <NavLink>
@@ -53,6 +66,23 @@ function Header() {
             </NavLink>
           </li>
         </ul>
+      </div>
+      <div
+        className={`block transform md:hidden  ${show ? "rotate-180" : ""}`}
+        onClick={() => setShow(!show)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-7 h-7 stroke-2"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+            clip-rule="evenodd"
+          />
+        </svg>
       </div>
     </header>
   );
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,10 +5,14 @@ import ProductLoad from "../component/ProductLoad";
 function Category() {
   const [category, setCategory] = useState([]);
   const [product, setProduct] = useState([]);
+  const [productCopy, setProductCopy] = useState([]);
   const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+  const [sortOpen, setSortOpen] = useState(false);
   const [categoryType, setCategoryType] = useState("all");
   const [loading, setLoading] = useState(false);
+  const [sortBy, setSortBy] = useState("relevent");
   const categotyRef = useRef(null);
+  const sortRef = useRef(null);
   useEffect(() => {
     axios
       .get("https://dummyjson.com/products/categories")
@@ -26,12 +30,13 @@ function Category() {
         .get(url)
         .then((data) => {
           setProduct(data?.data?.products);
+          setProductCopy(data?.data?.products);
           setLoading(false);
         })
         .catch((error) => console.log(error));
     }
     if (categoryType == "all") {
-      getCategoryProduct("https://dummyjson.com/products?limit=8&skip=10");
+      getCategoryProduct("https://dummyjson.com/products?limit=8&skip=1");
     } else {
       getCategoryProduct(
         `https://dummyjson.com/products/category/${categoryType}`
@@ -39,12 +44,23 @@ function Category() {
     }
   }, [categoryType]);
 
-  console.log(product);
-  //   console.log(categotyRef.current.scrollHeight);
   function toggleCategory() {
     setIsCategoryOpen((prev) => !prev);
   }
-  console.log(isCategoryOpen);
+  function toggleSort() {
+    setSortOpen((prev) => !prev);
+  }
+
+  useEffect(() => {
+    if (sortBy == "relevent") {
+      setProduct([...productCopy]);
+    } else if (sortBy == "highToLow") {
+      setProduct([...product.sort((a, b) => b.price - a.price)]);
+    } else if (sortBy == "lowToHigh") {
+      setProduct([...product].sort((a, b) => a.price - b.price));
+    }
+  }, [sortBy, productCopy]);
+
   return (
     <div className="w-full flex p-5 gap-4">
       <div className="w-[300px]  bg-gray-100 shadow-lg rounded-sm">
@@ -81,19 +97,72 @@ function Category() {
             <p
               key={`categoty ${item}`}
               onClick={(e) => setCategoryType(e.target.innerText.toLowerCase())}
-              className="mt-1 pb-1 cursor-pointer hover:text-primary hover:font-semibold"
+              className="mt-1 pb-1 cursor-pointer hover:text-primary text-slate-600  text-[14px] font-semibold"
             >
               {item.toUpperCase()}
             </p>
           ))}
         </div>
+        <div>
+          <h2
+            className="font-semibold flex justify-between py-2 px-7 rounded-sm bg-slate-500 mt-5 text-white cursor-pointer "
+            onClick={toggleSort}
+          >
+            <span>Sort Price By</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className={`w-6 duration-150 transition-all ease-in-out h-6 ${
+                sortOpen ? "rotate-180" : 0
+              }`}
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </h2>
+          <div
+            ref={sortRef}
+            className="bg-white   pl-10 overflow-hidden transition-max-height duration-500 ease-in-out text-slate-700 text-[15px]"
+            style={{
+              maxHeight: `${sortOpen ? sortRef.current.scrollHeight : 0}px`,
+            }}
+          >
+            <button
+              className="py-1 mt-1 duration-200 hover:text-primary block  font-semibold"
+              onClick={() => setSortBy("relevent")}
+            >
+              Relevent
+            </button>
+            <button
+              onClick={() => setSortBy("highToLow")}
+              className="py-1 mt-1 block duration-200 hover:text-primary  font-semibold"
+            >
+              High to Low
+            </button>
+            <button
+              onClick={() => setSortBy("lowToHigh")}
+              className="pb-5 py-1 mt-1 duration-200 hover:text-primary block  font-semibold"
+            >
+              Low To High
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="w-[calc(100vw-300px)] border grid md:grid-cols-3 gap-10  rounded-sm">
-        {loading
-          ? Array(6)
-              .fill("")
-              .map((_, index) => <ProductLoad />)
-          : product?.map((item, index) => <Card card={item} />)}
+      <div className="w-[calc(100vw-300px)]">
+        <h2 className="font-semibold text-2xl py-1 mb-2 ml-10">
+          {categoryType.toLocaleUpperCase()}
+        </h2>
+        <div className="w-full  grid md:grid-cols-3 gap-10  rounded-sm">
+          {loading
+            ? Array(6)
+                .fill("")
+                .map((_, index) => <ProductLoad />)
+            : product?.map((item, index) => <Card card={item} />)}
+        </div>
       </div>
     </div>
   );
